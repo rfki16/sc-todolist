37fe434a0fb32f83e740a6b19752f20b42f20e6d
rfki16 <achmadrifki089@gmail.com>
2023-05-02 18:40:46 +0700

    show

--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -3,4 +3,10 @@
 // Menampilkan Todolist
 function showTodoList()
 {
+    global $todoList;
+    echo "TODOLIST APPS".PHP_EOL;
+
+    foreach($todoList as $number => $value){
+        echo "$number . $value" . PHP_EOL;
+    }
 }
