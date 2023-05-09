b37535a59c2a365dcec7686fd024fce3c7c08df1
rfki16 <achmadrifki089@gmail.com>
2023-05-09 13:57:57 +0700

    view

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,1 +1,38 @@
 <?php
+
+require_once './Helper/Input.php';
+require_once './BusinessLogic/ShowTodoList.php';
+require_once 'ViewAddTodoList.php';
+require_once 'ViewAddTodoList.php';
+require_once 'ViewAddTodoList.php';
+
+
+function viewShowTodoList()
+{
+    while (true) {
+
+    showTodoList();
+
+    echo "MENU TO DO LIST";
+    echo "1. Tambah Todo" . PHP_EOL;
+    echo "2. Hapus Todo" . PHP_EOL;
+    echo "x. Keluar" . PHP_EOL;
+
+    $pilihan = input("Pilih");
+
+    if ($pilihan == "1")
+    {
+        viewAddTodoList();
+    } else if ($pilihan == "2")
+    {
+        viewRemoveTodoList();
+    } else if ($pilihan == "x")
+    {
+        break;
+    } else{
+        echo "Pilihan tidak ada";
+    }
+    }
+    echo "Sampai jumpa lagi";
+
+}
