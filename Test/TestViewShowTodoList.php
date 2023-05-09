e2bbe260c74412e7b43baf1f2947b803484a9194
rfki16 <achmadrifki089@gmail.com>
2023-05-09 13:58:14 +0700

    test view

---
+++ Test/TestViewShowTodoList.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once './View/ViewShowTodoList.php';
+require_once './BusinessLogic/AddTodoList.php';
+
+addTodoList("Achmad");
+addTodoList("Rifki");
+addTodoList("Malik");
+
+viewShowTodoList();
